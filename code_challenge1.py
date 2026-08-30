73ca3eb71b447706f18ab1e38c31213d59a95bc9
miguelornedo17 <multivitamind177@gmail.com>
2026-08-30 22:32:57 +0800

    CHALLENGE

--- code_challenge1.py
+++ code_challenge1.py
@@ -1,1 +1,2 @@
-print("\t\t\t\t\t\t\t*\n\t\t\t\t\t\t*\t\t*\n\t\t\t\t\t*\t\t\t\t*\n\t\t\t\t* \t\t\t\t\t\t*\n\t\t\t* \t\t\tHi\\, My Name is \t\t \n\t\t*\t\t\\\"Miguel\"\\")
+#!/usr/bin/python3
+print("\t\t\t\t\t\t\t\t*\n\t\t\t\t\t\t\t*\t\t*\n\t\t\t\t\t\t*\t\t\t\t*\n\t\t\t\t\t* \t\t\t\t\t\t*\n\t\t\t\t* \t\t\tHi\\, My Name is \t\t\t* \n\t\t\t*\t\t\t\t\\\"Miguel\"\\\t\t\t\t\t*\n\t\t\t\t*\t\t\t\t\t\t\t\t*\n\t\t\t\t\t*\t\t\t\t\t\t*\n\t\t\t\t\t\t*\t\t\t\t*\n\t\t\t\t\t\t\t*\t\t*\n\t\t\t\t\t\t\t\t*")
