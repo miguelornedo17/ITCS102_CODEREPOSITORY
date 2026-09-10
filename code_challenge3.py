411a87f592568956276397891a7b2fe11da55585
miguelornedo17 <multivitamind177@gmail.com>
2026-09-10 16:32:39 +0800

    Challenge
    Refactored shipping fee calculation logic and input prompts.

--- code_challenge3.py
+++ code_challenge3.py
@@ -1,17 +1,36 @@
 sender = input("Sender Name --> ")
 item = input("Type of item --> ")
-isFragile = bool(input("is it fragile --> "))
+isFragile = bool(input("is it fragile (if not press Enter) --> "))
 weight = float(input("Weight of the item in kg --> "))
 distance = float(input("Distance in km --> "))
-is_express = bool(input("Express --> "))
-is_international = bool(input("International --> "))
+is_express = bool(input("Express (if not press Enter) --> "))
+is_international = bool(input("International (if not press Enter) --> "))
+
+#Calculation steps
+base_cost = (weight * 2.50) + (distance * 0.15)
 
 if weight <= 2 and distance <= 100 and is_express == False and is_international == False : 
 	print("Shipping fee is Free")
-elif is_international 
+	Total = 0
 
-base_cost = (weight * 2.50) + (distance * 0.15)
-if isFragile == True :
-	print("Fragile") 
-else : 
-	print("Not Fragile")
+elif is_international == True and is_express == True :
+	print("International Express Product")
+	Total = (base_cost * 1.40) + 50
+	
+elif is_international == True and weight >= 20 or is_express == True :
+	print("The Product is Express or Heavy international")
+	Total = (base_cost * 1.20) + 25
+
+elif weight >= 30 or distance >= 1000 :
+	print("Oversized Weight")
+	Total = base_cost + 30
+
+else :
+	print("Standard Rate")
+	Total = base_cost 
+
+print("---------------------")
+print("Name of the sender --> ", sender)
+print("Product name --> ", item)
+print("Total Price --> PHP", Total)
+
