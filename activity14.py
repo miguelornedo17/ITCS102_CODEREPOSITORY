095fae6d0290830c7314b0229a4a0c212504ba26
miguelornedo17 <multivitamind177@gmail.com>
2026-09-15 16:05:28 +0800

    Challenge

--- activity14.py
+++ activity14.py
@@ -8,11 +8,11 @@ age = int(input("Enter your age ---> "))
 is_employed = bool(input("Are you employed? (True/False) ---> "))
 credit_score = int(input("Entere your credit score --> "))
 annual_income = float(input("Enter your annual income ---> "))
-has_collateral = bool((input("Do you havr collateral ? (True/False) --->"))
+has_collateral = bool(input("Do you havr collateral ? (True/False) --->"))
 
 base_rate = 0.0
 if age >= 21 and is_employed == True :
-    print("Applicant pass the baseline rewuirment"))
+    print("Applicant pass the baseline rewuirment")
     if credit_score >= 750:
         base_rate = 5.0
         print("You have a high vredit score")
@@ -25,10 +25,14 @@ if age >= 21 and is_employed == True :
         base_rate = 8.0
         print("Youy have a medium credit score")
         if has_collateral == True:
-            base_rate = 
-            pass
+            base_rate = 7.0
+            print("You have medium annual income and credit score", base_rate)
+        if annual_income < 40000 :
+            base_rate = 9.5
+            print("You have a medium annual income and credit score", base_rate)
+    elif credit_score < 600 :
+        print("Rejected: Credit score too low")
     else:
         pass
-    
 else:
-    pass
+    print("Rejected: Fails baseline Criteria")
